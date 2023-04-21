c8a2857097f6c29e628b8917b5e4d30a49a42215
Tzvi (Steve) Neiman <tneiman19@gmail.com>
2023-04-21 16:37:13 -0400

    Created landing page route with  posts

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,4 +3,17 @@ const sequelize = require("../config/connection.js");
 const { Post, User, Answer } = require("../models");
 const withAuth = require("../utils/auth");
 
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.findAll({
+      include: [User],
+      raw: true,
+    });
+
+    res.render("posts", { posts });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
 module.exports = router;
