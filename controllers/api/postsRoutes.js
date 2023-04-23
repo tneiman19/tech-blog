f37807779615a81111a1a8c940d5d170205d0b07
Tzvi (Steve) Neiman <tneiman19@gmail.com>
2023-04-23 12:58:31 -0400

    Created Routes in controller folder

--- controllers/api/postsRoutes.js
+++ controllers/api/postsRoutes.js
@@ -2,4 +2,43 @@ const router = require("express").Router();
 const { Post } = require("../../models");
 const withAuth = require("../../utils/auth");
 
+router.post("/", withAuth, async (req, res) => {
+  const body = req.body;
+
+  try {
+    const newPost = await Post.create({ ...body, userId: req.session.userId });
+    res.json(newPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put("/:id", withAuth, async (req, res) => {
+  try {
+    const [affectedRows] = await Post.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    affectedRows > 0 ? res.status(200).end() : res.status(404).end();
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/:id", withAuth, async (req, res) => {
+  try {
+    const [affectedRows] = Post.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    affectedRows > 0 ? res.status(200).end() : res.status(404).end();
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
